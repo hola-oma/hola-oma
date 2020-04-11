e358a30ab926df33a177e26157e2b509ac2f3154
Kristin <kristin.wood@eduworks.com>
2020-04-11 15:42:17 -0400

    File Upload

--- src/shared/features/NewFamilyPost/NewFamilyPost.tsx
+++ src/shared/features/NewFamilyPost/NewFamilyPost.tsx
@@ -1,14 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import {Box} from '@material-ui/core';
 
 import './NewFamilyPost.css';
 
 const NewFamilyPost: React.FC = () => {
+    const [selectedFile, onSelect] = useState<File | null>();
 
     return (
         <>
-
+        <Box>
+            <input type="file" onChange={(event) => onSelect(event.target.files ? event.target.files[0] : null)}></input>
+        </Box>
          <Box className="todo">
             <h3>To do items:</h3>
             <ul>
