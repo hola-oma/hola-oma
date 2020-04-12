462e64859463125fa0a459dbaab27fa3cd2c4663
Mandi <mandi018@gmail.com>
2020-04-12 13:56:40 -0500

    Fixed missing Google login codeblock

--- src/shared/features/Login.tsx
+++ src/shared/features/Login.tsx
@@ -51,7 +51,6 @@ const Login: React.FC<ILogin> = ({ history }) => {
     }
   };
 
-/*
   const handleGoogleLogin = async () => {
     try {
       const loggedInUser = await signUserInWithGoogle();
@@ -60,7 +59,7 @@ const Login: React.FC<ILogin> = ({ history }) => {
     } catch(e) {
       setErrors(e.message);
     }
-*/
+  }
 
   const useStyles = makeStyles((theme) => ({
     paper: {
