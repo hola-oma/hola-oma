295722802e16a0cf40be81b088e47179ca9fa873
Rebecca <tayloreb@oregonstate.edu>
2020-04-13 14:43:49 -0500

    Allow for different ".where" query options

--- src/services/post.ts
+++ src/services/post.ts
@@ -1,7 +1,10 @@
+//Resource: https://stackoverflow.com/questions/48036975/firestore-multiple-conditional-where-clauses
+
 import * as firebase from "firebase/app";
 import {Post} from '../shared/models/post.model';
 import {authenticateFromStore} from "./user";
 import {roles} from "../enums/enums";
+import FieldPath from "firebase/firestore";
 
 export const getPosts = async (role: roles): Promise<Post[]> => {
   await authenticateFromStore();
@@ -9,53 +12,29 @@ export const getPosts = async (role: roles): Promise<Post[]> => {
   const db = firebase.firestore();
   const posts: Array<Post> = [];
 
+  // Get user id
   let userId = user?.uid;
-  console.log("role passed in as parameter: " + role);
   console.log("user id: " + userId);
 
-  // Get posts where grandparent is recipient
-  if (role === roles.receiver) {
-    console.log("showing receiver posts");
-    await db.collection("posts")
-      .where("receiverIDs", "array-contains", user?.uid).get()
-      .then((snapshot) => {
-        if (snapshot.empty) {
-          console.log("No grandparent posts found");
-          return;
-        }
-
-        snapshot.forEach(doc => {
-          //console.log(doc.id, '->', doc.data());
-          let data = doc.data();
-          posts.push({
-            id: data.id,
-            creatorID: data.creatorID,
-            from: data.from,
-            message: data.message,
-            photoURL: data.photoURL,
-            read: data.read
-          });
-        });
-      })
-      .catch(err => {
-        console.log('Error getting documents', err);
-      });
+  // Set query options based on whether user is a poster or a receiver
+  let queryOptions = [ [<string>"receiverIDs", "array-contains"], ["users", "=="] ];
+  let queryWhere = queryOptions[0];     // for receiver
+  if (role === roles.poster) {          // else if poster
+    queryWhere = queryOptions[1];
   }
-  return posts;
-}
 
-/*
-  // Get posts sent by family member
+  // Get posts
   await db.collection("posts")
-    .where("users", "==", user?.uid).get()
+    .where(<string>queryWhere[0],                 // FieldPath
+      <"==" | "array-contains-any">queryWhere[1], // string Opt
+      userId).get()
     .then((snapshot) => {
       if (snapshot.empty) {
-        console.log("No posts found");
+        console.log("No posts found for: " + role + ", userID: " + userId);
         return;
       }
-
       snapshot.forEach(doc => {
-        //console.log(doc.id, '->', doc.data());
+      //console.log(doc.id, '->', doc.data());
         let data = doc.data();
         posts.push({
           id: data.id,
@@ -70,8 +49,7 @@ export const getPosts = async (role: roles): Promise<Post[]> => {
     .catch(err => {
       console.log('Error getting documents', err);
     });
-  
   return posts;
-};
-*/
+}
+
 // todo: createPost
