895271c55dc59ec1a25b699857680f2e51bcc051
Mandi <mandi018@gmail.com>
2020-04-20 14:25:49 -0500

    Can only access routes via history pushes, so I added a button to the header to get here until it's part of the post reply feature

---
+++ src/shared/features/PhotoReplyPrototype.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+interface IPhotoReplyPrototype {
+  // empty for now, just need this for the "extends RouteComponentProps" part 
+}
+
+
+const PhotoReplyPrototype: React.FC<IPhotoReplyPrototype> = ({ }) => {
+
+  return (
+   <div>
+     <h1>Test photo reply here</h1>
+   </div>
+  );
+};
+
+export default PhotoReplyPrototype;
