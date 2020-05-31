c1f7e6a4d797b2a3840c2d34e52d27a08e827f85
Kristin <kristin.wood@eduworks.com>
2020-05-31 18:41:06 -0400

    Pass in post id rather than post, so edits persist after refreshing page

--- src/shared/features/FamilyMsgView/FamilyMsgView.tsx
+++ src/shared/features/FamilyMsgView/FamilyMsgView.tsx
@@ -6,7 +6,7 @@ import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
 
 import { Post } from 'shared/models/post.model';
 import { getLinkedAccounts } from "services/accountLink";
-import { deletePost } from "services/post";
+import { deletePost, getPostById } from "services/post";
 import { getRepliesToPost, markReplyRead } from "services/reply";
 import { Reply } from "../../models/reply.model";
 import { replyEmojiPNGs } from "../../../Icons";
@@ -73,7 +73,7 @@ interface ISubProps {
 }
 
 interface ISubSubProps {
-    post: Post
+    postId: string
 }
 
 interface IReceiver {
@@ -84,7 +84,7 @@ interface IReceiver {
 const FamilyMsgView: React.FC<IFamilyMsgView> = (props) => {
     const classes = useStyles();
     const [editModalOpen, setEditModalOpen] = useState(false);
-    const [post, setPost] = useState(props.location.state.post);
+    const [post, setPost] = useState<Post>();
     const [receivers, setReceivers] = useState<IReceiver[]>([]);
     const [replies, setReplies] = useState<Reply[]>([]);
     const [repliesSet, setRepliesSet] = useState<boolean>(false);
@@ -106,42 +106,52 @@ const FamilyMsgView: React.FC<IFamilyMsgView> = (props) => {
     }
 
     useEffect(() => {
-        //Get connected accounts to populate receiver list
-        getLinkedAccounts()
-        .then((linkedAccounts) => {
-            let rcvrs = [];
-            for (let i = 0; i < linkedAccounts.length; i++) {
-                if (linkedAccounts[i].verified === true) {
-                    let displayName = linkedAccounts[i].displayName ? linkedAccounts[i].displayName : "Unknown Username";
-                    let receiver = {
-                        id: linkedAccounts[i].id,
-                        name: displayName};
-                    rcvrs.push(receiver);
-                }
-            }
-            setReceivers(rcvrs.sort(sortNames));
+        //Get post from id
+        let id = props.location.state.postId;
+        getPostById(id).then((returnedPost) => {
+            setPost(returnedPost);
         });
-        getRepliesToPost(post.pid).then((replyArray: any) => {
-            let replies: Reply[];
-            replies = [];
-            let emojiReplies: Array<Array<string>>;
-            emojiReplies = [[], [], [], [], [], []];
-            for (let i = 0; i < replyArray.length; i++) {
-                markReplyRead(replyArray[i].rid);
-                if (replyArray[i].replyType === "emoji") {
-                    for (let j = 0; j < replyArray[i].message.length; j++) {
-                        let emojiIndex = replyArray[i].message[j];
-                        emojiReplies[emojiIndex].push(replyArray[i].from);
+    }, [props.location.state.postId]); // fires on page load if this is empty [] 
+
+    useEffect(() => {
+        if (post) {
+            //Get connected accounts to populate receiver list
+            getLinkedAccounts()
+            .then((linkedAccounts) => {
+                let rcvrs = [];
+                for (let i = 0; i < linkedAccounts.length; i++) {
+                    if (linkedAccounts[i].verified === true) {
+                        let displayName = linkedAccounts[i].displayName ? linkedAccounts[i].displayName : "Unknown Username";
+                        let receiver = {
+                            id: linkedAccounts[i].id,
+                            name: displayName};
+                        rcvrs.push(receiver);
                     }
-                } else {
-                    replies.push(replyArray[i]);
                 }
-            }
-            setReplies(replies);
-            setRepliesSet(true);
-            setEmojiReplies(emojiReplies);
-        });
-    }, [post.pid]); // fires on page load if this is empty [] 
+                setReceivers(rcvrs.sort(sortNames));
+            });
+            getRepliesToPost(post.pid).then((replyArray: any) => {
+                let replies: Reply[];
+                replies = [];
+                let emojiReplies: Array<Array<string>>;
+                emojiReplies = [[], [], [], [], [], []];
+                for (let i = 0; i < replyArray.length; i++) {
+                    markReplyRead(replyArray[i].rid);
+                    if (replyArray[i].replyType === "emoji") {
+                        for (let j = 0; j < replyArray[i].message.length; j++) {
+                            let emojiIndex = replyArray[i].message[j];
+                            emojiReplies[emojiIndex].push(replyArray[i].from);
+                        }
+                    } else {
+                        replies.push(replyArray[i]);
+                    }
+                }
+                setReplies(replies);
+                setRepliesSet(true);
+                setEmojiReplies(emojiReplies);
+            });
+        }
+    }, [post]); // fires on page load if this is empty [] 
 
     const handleEditModal = () => {
         setEditModalOpen(!editModalOpen);
@@ -162,9 +172,11 @@ const FamilyMsgView: React.FC<IFamilyMsgView> = (props) => {
     }
 
     const deleteCurrentPost = () => {
-        setConfirmDeleteModalOpen(false);
-        deletePost(post.pid);
-        history.push('/posts')
+        if (post) {
+            setConfirmDeleteModalOpen(false);
+            deletePost(post.pid);
+            history.push('/posts')
+        }
     }
 
     const messageAsString = (reply: Reply) => {
@@ -190,151 +202,158 @@ const FamilyMsgView: React.FC<IFamilyMsgView> = (props) => {
             </React.Fragment>
           );
     }
-
-    return (
-        <>
-        <Container>
-            <Typography component="h2" variant="h5" align="center">
-                Sent Message
-            </Typography>
-        </Container>
-        <Grid container alignItems="center">
-            <Grid item sm={3}></Grid>
-            <Grid item sm={6} xs={12}>
-                <Paper className={classes.paper}>
-                    <Column justify="center" alignItems="center">
-                        {post.photoURL && <img
-                            src={post.photoURL}
-                            className="photo"
-                            alt="Attached img"
-                        />}
-                        {post.videoURL && <video
-                            src={post.videoURL}
-                            preload="auto"
-                            controls
-                            className="photo"
-                        />}
-                        <br/>
-                        <Typography variant="h5" className={[classes.message, "wrapReply"].join(' ')}>
-                            {post.message}
-                        </Typography>
-                    </Column>
-                </Paper>
-            </Grid>
-            <Grid item sm={3} xs={12} className="padLeft">
-                <Column justify="center" alignItems="center">
-                    <Typography variant="caption" align="center">
-                        Sent <Moment format="MMMM Do YYYY, h:mm a">{post.date}</Moment>
-                        <br/>
-                        <br/>
-                        Seen by:
-                    </Typography>
-                    {
-                        receivers.map((receiver: IReceiver, index: number) => {
-                            return (
-                            <Grid container alignItems={receivers.length === 1 ? "center" : "flex-start"} justify={receivers.length === 1 ? "center" : "flex-start"} key={index}>
-                                {post.read[receiver.id] === true ? <CheckBoxIcon fontSize="small"/> : <CheckBoxOutlineBlankIcon fontSize="small"/>}
-                                <Typography variant="caption" align="center">
-                                    {receiver.name}
-                                </Typography>
-                            </Grid>
-                            )
-                        })
-                    }
-                    <br/>
-                    <br/>
-                    <Button
-                        variant="contained"
-                        size="small"
-                        color="secondary"
-                        className={classes.spacing}
-                        startIcon={<EditIcon />}
-                        onClick={handleEditModal}>
-                        Edit Post
-                    </Button>
-                    <Button
-                        variant="outlined"
-                        color="primary"
-                        size="small"
-                        className={classes.spacing}
-                        startIcon={<DeleteIcon />}
-                        onClick={onClickDelete}>
-                        Delete Post
-                    </Button>
-                </Column>
-            </Grid>
-        </Grid>
-        <Container>
-            <hr />
-            <Typography component="h2" variant="h5" align="center">
-                Replies
-            </Typography>
-            <Row alignItems="center" justify="center">
-                {emojiReplies && emojiReplies.map((reply, index: number) => {
-                    return reply.length > 0 && (
-                        <Tooltip title={getTooltip(reply)} key={index} arrow>
-                            <img
-                                src={emojiIcons[index]}
-                                className={classes.emojis}
-                                alt="Emoji reply"
-                            />
-                        </Tooltip>
-                    )
-                })}
-            </Row>
-            <Grid container className={classes.bottomMargin}>
-                {
-                replies.map((reply: Reply, index: number) => {
-                    return (
-                    <Child xs={12} key={index}>
-                        <div className={classes.postStyle}>
-                            <Card variant="outlined" className="replyCard">
-                                <CardContent className="replyContent">
-                                    {isMessage(reply) && 
-                                        <p>{reply.message}</p>
-                                    }
-
-                                    {isPhoto(reply) &&
-                                        <img
-                                            src={messageAsString(reply)}
-                                            className="photo"
-                                            alt="Reply img"
-                                        />
-                                    }
-                                </CardContent>
-                                <CardActions className="replyActions">
-                                    <Typography variant="caption">
-                                        Sent by {reply.from}
-                                        <br/>
-                                        <Moment format="MMMM Do YYYY, h:mm a">{reply.date}</Moment>
-                                    </Typography>
-                                </CardActions>
-                            </Card>
-                        </div>
-                    </Child>
-                    )
-                })
-                }
-            </Grid>
-            {(repliesSet && replies.length === 0) &&
-                <Typography variant="caption" style={{display:'flex',alignItems:'center',justifyContent:'center'}}>
-                    You have no message or photo replies to this post.
+    if (post) {
+        return (
+            <>
+            <Container>
+                <Typography component="h2" variant="h5" align="center">
+                    Sent Message
                 </Typography>
-            }
-            <Modal open={editModalOpen} onClose={handleEditModal} style={{display:'flex',alignItems:'center',justifyContent:'center'}}>
-            <div className={classes.editModal}>
-                {editPost && <NewFamilyPost currentPost={post} closeModal={doneEditing}/>}
-            </div>
-            </Modal>
-        </Container>
+            </Container>
+            <Grid container alignItems="center">
+                <Grid item sm={3}></Grid>
+                <Grid item sm={6} xs={12}>
+                    <Paper className={classes.paper}>
+                        <Column justify="center" alignItems="center">
+                            {post.photoURL && <img
+                                src={post.photoURL}
+                                className="photo"
+                                alt="Attached img"
+                            />}
+                            {post.videoURL && <video
+                                src={post.videoURL}
+                                preload="auto"
+                                controls
+                                className="photo"
+                            />}
+                            <br/>
+                            <Typography variant="h5" className={[classes.message, "wrapReply"].join(' ')}>
+                                {post.message}
+                            </Typography>
+                        </Column>
+                    </Paper>
+                </Grid>
+                <Grid item sm={3} xs={12} className="padLeft">
+                    <Column justify="center" alignItems="center">
+                        <Typography variant="caption" align="center">
+                            Sent <Moment format="MMMM Do YYYY, h:mm a">{post.date}</Moment>
+                            <br/>
+                            <br/>
+                            Seen by:
+                        </Typography>
+                        {
+                            receivers.map((receiver: IReceiver, index: number) => {
+                                return (
+                                <Grid container alignItems={receivers.length === 1 ? "center" : "flex-start"} justify={receivers.length === 1 ? "center" : "flex-start"} key={index}>
+                                    {post.read[receiver.id] === true ? <CheckBoxIcon fontSize="small"/> : <CheckBoxOutlineBlankIcon fontSize="small"/>}
+                                    <Typography variant="caption" align="center">
+                                        {receiver.name}
+                                    </Typography>
+                                </Grid>
+                                )
+                            })
+                        }
+                        <br/>
+                        <br/>
+                        <Button
+                            variant="contained"
+                            size="small"
+                            color="secondary"
+                            className={classes.spacing}
+                            startIcon={<EditIcon />}
+                            onClick={handleEditModal}>
+                            Edit Post
+                        </Button>
+                        <Button
+                            variant="outlined"
+                            color="primary"
+                            size="small"
+                            className={classes.spacing}
+                            startIcon={<DeleteIcon />}
+                            onClick={onClickDelete}>
+                            Delete Post
+                        </Button>
+                    </Column>
+                </Grid>
+            </Grid>
+            <Container>
+                <hr />
+                <Typography component="h2" variant="h5" align="center">
+                    Replies
+                </Typography>
+                <Row alignItems="center" justify="center">
+                    {emojiReplies && emojiReplies.map((reply, index: number) => {
+                        return reply.length > 0 && (
+                            <Tooltip title={getTooltip(reply)} key={index} arrow>
+                                <img
+                                    src={emojiIcons[index]}
+                                    className={classes.emojis}
+                                    alt="Emoji reply"
+                                />
+                            </Tooltip>
+                        )
+                    })}
+                </Row>
+                <Grid container className={classes.bottomMargin}>
+                    {
+                    replies.map((reply: Reply, index: number) => {
+                        return (
+                        <Child xs={12} key={index}>
+                            <div className={classes.postStyle}>
+                                <Card variant="outlined" className="replyCard">
+                                    <CardContent className="replyContent">
+                                        {isMessage(reply) && 
+                                            <p>{reply.message}</p>
+                                        }
 
-        <ManageConfirmDelete 
-            isOpen={confirmDeleteModalOpen} 
-            deleteConfirmed={() => deleteCurrentPost()}
-            onClose={handleConfirmDeleteModalClose} 
-        />
-     </>
-    )
+                                        {isPhoto(reply) &&
+                                            <img
+                                                src={messageAsString(reply)}
+                                                className="photo"
+                                                alt="Reply img"
+                                            />
+                                        }
+                                    </CardContent>
+                                    <CardActions className="replyActions">
+                                        <Typography variant="caption">
+                                            Sent by {reply.from}
+                                            <br/>
+                                            <Moment format="MMMM Do YYYY, h:mm a">{reply.date}</Moment>
+                                        </Typography>
+                                    </CardActions>
+                                </Card>
+                            </div>
+                        </Child>
+                        )
+                    })
+                    }
+                </Grid>
+                {(repliesSet && replies.length === 0) &&
+                    <Typography variant="caption" style={{display:'flex',alignItems:'center',justifyContent:'center'}}>
+                        You have no message or photo replies to this post.
+                    </Typography>
+                }
+                <Modal open={editModalOpen} onClose={handleEditModal} style={{display:'flex',alignItems:'center',justifyContent:'center'}}>
+                <div className={classes.editModal}>
+                    {editPost && <NewFamilyPost currentPost={post} closeModal={doneEditing}/>}
+                </div>
+                </Modal>
+            </Container>
+
+            <ManageConfirmDelete 
+                isOpen={confirmDeleteModalOpen} 
+                deleteConfirmed={() => deleteCurrentPost()}
+                onClose={handleConfirmDeleteModalClose} 
+            />
+        </>
+        )
+    } else {
+        return (
+            <>
+            Post not found.
+            </>
+        )
+    }
 };
 
 export default FamilyMsgView;
